a235638c22f9317d267c4613f326d1c6c0f96816
Ajay0503 <ajayshanker533@gmail.com>
2025-01-11 15:10:07 +0530

    till day5

---
+++ comment.escape-sequence.py
@@ -0,0 +1,6 @@
+print("Hello World")
+print(5)
+print(5*6)
+print("sdfgh \"sdfgfdd\" \n sdfg") #is escape sequence(\n new line)
+#single line comment is done by using # symbol
+print("asdfg",5,"sdfg",sep="~",end="009\n")
