1702fd80e74c5ae51498bbdf5b1f15bf9b17130b
Ajay0503 <ajayshanker533@gmail.com>
2025-01-15 23:48:15 +0530

    .

---
+++ Day28 - f strings.py
@@ -0,0 +1,8 @@
+name="Ajay"
+age=25  
+print(f"Hey, {name}! You are {age} years old.")
+
+txt =f"Name is {name} and age is {age}"
+print(txt)
+
+print(type(f"{name}"))
---
+++ Day29 - Docstrings.py
@@ -0,0 +1,6 @@
+def square(n):
+    """Return the square of 
+a number n."""
+#doc string should be direct after the function definition
+    return n**2
+print(square.__doc__)
